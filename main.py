c24f56890fe6a477c29ea1a50eb476704aeff486
Jason Lee <lijiansen.sjtu@gmail.com>
2018-03-06 20:54:54 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+# encoding: utf-8
+
+'''
+
+@author: Jason Lee
+
+@license: (C) Copyright @ Jason Lee
+
+@contact: jiansenll@163.com
+
+@file: main.py
+
+@time: 2018/3/6 20:48
+
+@desc:
+
+'''
+
